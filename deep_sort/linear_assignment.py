fcadd453e204588492bbcbe5aeb75b23bf606ca3
Alexander Dadukin <hi@st235.xyz>
2023-05-23 21:51:03 +0100

    Fix indicies out of range crash

--- deep_sort/linear_assignment.py
+++ deep_sort/linear_assignment.py
@@ -56,9 +56,11 @@ def min_cost_matching(
         tracks, detections, track_indices, detection_indices)
     cost_matrix[cost_matrix > max_distance] = max_distance + 1e-5
     indices = linear_assignment(cost_matrix)
+    indices = np.hstack([indices[0].reshape(((indices[0].shape[0]), 1)),indices[1].reshape(((indices[0].shape[0]), 1))])
 
     matches, unmatched_tracks, unmatched_detections = [], [], []
     for col, detection_idx in enumerate(detection_indices):
+        print(indices)
         if col not in indices[:, 1]:
             unmatched_detections.append(detection_idx)
     for row, track_idx in enumerate(track_indices):
