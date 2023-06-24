0b94d2a58019d9f663bf91e49c0ea031688e89cc
Alexander Dadukin <hi@st235.xyz>
2023-06-24 23:04:43 +0100

    Add benchmarking method to measure performance

---
+++ src/utils/benchmarking.py
@@ -0,0 +1,22 @@
+import time
+
+from functools import wraps
+from typing import Callable
+
+
+def benchmark(function: Callable) -> Callable:
+    """Measures a method performance.
+    """
+
+    instances_lookup = dict()
+
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        start_sec = time.time()
+        return_value = function(*args, **kwargs)
+        finish_sec = time.time()
+        elapsed_time_sec = finish_sec - start_sec
+        print(f"Elapsed time: {elapsed_time_sec} sec.")
+        return return_value
+
+    return wrapper
