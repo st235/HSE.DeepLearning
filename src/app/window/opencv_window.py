44da8c62bf2f88dd2ab88e7972dec170c344298d
Alexander Dadukin <hi@st235.xyz>
2023-06-25 16:44:47 +0100

    Fixed bug with window being black and open after it has been closed

--- src/app/window/opencv_window.py
+++ src/app/window/opencv_window.py
@@ -20,8 +20,9 @@ class OpenCVWindow(Window):
         # Due to a bug in OpenCV we must call imshow after destroying the
         # window. This will make the window appear again as soon as waitKey
         # is called.
+        # However, even after calling cv2.waitKey there are still no guarantees
+        # that the window will be closed at some GUI.
         #
         # see https://github.com/Itseez/opencv/issues/4535
         cv2.destroyWindow(self.title)
         cv2.waitKey(1)
-        cv2.imshow(self.title, np.zeros(self.size + (3, ), dtype=np.uint8))
